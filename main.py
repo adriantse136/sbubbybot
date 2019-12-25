85b98475077c6a62633524c89f4ac9b8744b0dc1
Sheen Patel <programmingpatel@gmail.com>
2019-12-25 15:21:06 -0500

    prelim work on flair part of bot

--- main.py
+++ main.py
@@ -3,15 +3,40 @@
 
 import praw #all the reddit i/o
 import threading #thread handler so multiple things can get done
+import os #to get enviroment variables containing secrets
+
+#create the reddit instance for the bot to use
+reddit = praw.Reddit(
+        user_agent='(SbubbyBot) v. 1.0',
+        client_id=os.environ["client_id"],
+        client_secret=os.environ['client_secret'],
+        username=os.environ['reddit_username'],
+        password=os.environ['reddit_password'])
+
+#mark the subreddit
+sbubby = reddit.subreddit('sbubby')
 
 def main():
     print("Running SbubbyBot testing version.")
-
-
+    doFlair()
+    print("Sbubbybot has finished running with no errors.")
 
 def doFlair():
     print("Flair system started...")
-
+    # continously monitor the stream of submitted posts.
+    for submission in sbubby.stream.submissions():
+        print(submission, "\n\tflair: ", submission.link_flair_text, "\n\tpost title: ", submission.title, "\n\t")
+        if submission.link_flair_text == "None":
+            print("This post needs a flair!")
+            #check to see if post already been messaged
+            hasBeenMessaged = False
+            for comment in submission.comments:
+                if comment.author == reddit.user.me(): #if i have a top level comment then don't message
+                    hasBeenMessaged = True
+            if not hasBeenMessaged:
+                print("message this post to remind to flair!!!")
+            else:
+                print("no need for flair!")
 def doMagicEye():
     print("MAGIC_EYE_BOT comment checker started...")
 
@@ -27,5 +52,5 @@ def commonRepost():
 def sundaySbubby():
     print("It is sunday. Sunday Sbubby started...")
 
-if __name__ == __main__:
+if __name__ == "__main__":
     main()
