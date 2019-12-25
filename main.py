66b0f1ffb4231b33d5751412b9dbc443efd2b573
PseudonymPatel <programmingpatel@gmail.com>
2019-12-25 17:31:37 +0000

    add to github

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 #Sheen Patel
 #u/SbubbyBot for the the mods of r/Sbubby
 
+import praw #all the reddit i/o
+import threading #thread handler so multiple things can get done
+
 def main():
-    print("hi")
+    print("Running SbubbyBot testing version.")
 
 
+
+def doFlair():
+    print("Flair system started...")
+
+def doMagicEye():
+    print("MAGIC_EYE_BOT comment checker started...")
+
+def howMuchKarmaModmail():
+    print("Anti-\"how much karma\" bot started...")
+
+def commonRepost():
+    print("Common reposts bot started...")
+    #Check each item in the imgur album -- if any is over the threshold:
+    #   make a comment with the similarity amount, and then give link that it is similar to.
+    #   mark post as spam(, and if user replies, then send modmail?)
+
+def sundaySbubby():
+    print("It is sunday. Sunday Sbubby started...")
 
 if __name__ == __main__:
     main()
